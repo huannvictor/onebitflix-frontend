bace82f03f53dcaa52535c5797c683b3425c20a9
Huann Almeida <huannvictor@gmail.com>
2023-04-22 03:32:01 -0300

    Manipulação visual dos slides

--- src/components/HomeNoAuth/slideSection/index.tsx
+++ src/components/HomeNoAuth/slideSection/index.tsx
@@ -14,7 +14,7 @@ interface props {
 const SlideSection = ({ newestCourses }: props) => {
   return (
     <>
-      <Container fluid>
+      <Container fluid className="d-flex flex-column align-items-center py-5">
         <p className={styles.sectionTitle}>AULAS JÁ DISPONÍVEIS</p>
         <SlideComponent course={newestCourses} />
         <Link href="/register">

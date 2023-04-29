1aa7e0e940f5ff35f8db012a75c33897b6f296d2
Huann Almeida <huannvictor@gmail.com>
2023-04-29 16:53:43 -0300

    iniciando conexão com backend

--- src/components/profile/user/index.tsx
+++ src/components/profile/user/index.tsx
@@ -1,15 +1,84 @@
 /* eslint-disable @next/next/no-img-element */
+import { FormEvent, useEffect, useState } from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import ToastComponent from "@/components/common/toast";
 
+import profileService from "@/services/profileService";
 import styles from "../../../styles/profile.module.scss";
 
 const UserForm = () => {
+  const [color, setColor] = useState("");
+  const [toastIsOpen, setToastIsOpen] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [created_at, setCreated_at] = useState("");
+
+  const [savedFirstName, setSavedFirstName] = useState("");
+  const [savedLastName, setSavedLastName] = useState("");
+
+  const [abbreviation, setAbbreviation] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  useEffect(() => {
+    profileService.fetchCurrent().then((user) => {
+      setFirstName(user.firstName);
+      setLastName(user.lastName);
+      setSavedFirstName(user.firstName);
+      setSavedLastName(user.lastName);
+      setPhone(user.phone);
+      setEmail(user.email);
+      setCreated_at(user.createdAt);
+    });
+  }, []);
+
+  const handleUserUpdate = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const res = await profileService.userUpdate({
+      firstName,
+      lastName,
+      phone,
+      email,
+      created_at,
+    });
+
+    if (res !== 200) {
+      setToastIsOpen(true);
+      setToastMessage("Você não pode alterar para este email.");
+      setColor("bg-danger");
+      setTimeout(() => {
+        setToastIsOpen(false);
+      }, 3000);
+
+      return;
+    }
+
+    setFullName(`${firstName} ${lastName}`);
+    setAbbreviation(`${firstName[0]}${lastName[0]}`);
+
+    setToastIsOpen(true);
+    setToastMessage("Informações alteradas com sucesso!");
+    setColor("bg-success");
+    setTimeout(() => {
+      setToastIsOpen(false);
+    }, 3000);
+  };
+
   return (
     <>
-      <Form className={styles.form}>
+      <Form onSubmit={handleUserUpdate} className={styles.form}>
         <div className={styles.formName}>
-          <p className={styles.nameAbbreviation}>HV</p>
-          <p className={styles.userName}>HUANN ALMEIDA</p>
+          <p className={styles.nameAbbreviation}>
+            {abbreviation
+              ? `${abbreviation}`
+              : `${savedFirstName[0]}${savedLastName[0]}`}
+          </p>
+          <p className={styles.userName}>
+            {fullName ? fullName : `${savedFirstName} ${savedLastName}`}
+          </p>
         </div>
         <div className={styles.membershipTime}>
           <img
@@ -38,7 +107,8 @@ const UserForm = () => {
               required
               maxLength={20}
               className={styles.inputFlex}
-              defaultValue={"John"}
+              defaultValue={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
             />
           </FormGroup>
           <FormGroup>
@@ -53,7 +123,8 @@ const UserForm = () => {
               required
               maxLength={20}
               className={styles.inputFlex}
-              defaultValue={"Doe"}
+              defaultValue={lastName}
+              onChange={(event) => setLastName(event.target.value)}
             />
           </FormGroup>
         </div>
@@ -61,16 +132,17 @@ const UserForm = () => {
         <div className={styles.inputContactDiv}>
           <FormGroup>
             <Label className={styles.label} for="phone">
-              WHATSAPP | TELEGRAM
+              WHATSAPP || TELEGRAM
             </Label>
             <Input
               name="phone"
-              type="tel"
               id="phone"
-              placeholder="+55 (××) ×××××-××××"
+              type="tel"
+              placeholder="+55 (DDD) 9XXXX XXXX"
               required
               className={styles.input}
-              defaultValue={"+55 (00) 99999-9999"}
+              defaultValue={phone}
+              onChange={(event) => setPhone(event.target.value)}
             />
           </FormGroup>
 
@@ -84,8 +156,9 @@ const UserForm = () => {
               id="email"
               placeholder="email@mail.com"
               required
-              className={styles.input}
-              defaultValue={"email@mail.com"}
+              className={styles.inputEmail}
+              defaultValue={email}
+              onChange={(event) => setEmail(event.target.value)}
             />
           </FormGroup>
 
@@ -94,6 +167,12 @@ const UserForm = () => {
           </Button>
         </div>
       </Form>
+
+      <ToastComponent
+        color={color}
+        isOpen={toastIsOpen}
+        message={toastMessage}
+      />
     </>
   );
 };

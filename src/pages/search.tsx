6a32dd265571566d93cc6eb6c4f63a77f2f1951e
Huann Almeida <huannvictor@gmail.com>
2023-04-30 23:08:45 -0300

    Criação da página de Search

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -8,6 +8,9 @@ import Head from "next/head";
 import HeaderAuth from "@/components/common/headerAuth";
 
 import styles from "../styles/search.module.scss";
+import SearchCard from "@/components/searchCard";
+import { Container } from "reactstrap";
+import Footer from "@/components/common/footer";
 
 const Search = () => {
   const router = useRouter();
@@ -32,13 +35,26 @@ const Search = () => {
         <title>OneBitFlix - {searchTerm}</title>
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
       </Head>
-      <main>
-        <HeaderAuth />
-        {searchResult?.map((course) => (
-          <div key={course.id}>
-            <p>{course.name}</p>
+      <main className={styles.main}>
+        <div className={styles.header}>
+          <HeaderAuth />
+        </div>
+        {searchResult.length > 0 ? (
+          <div className={styles.searchResult}>
+            <Container className="d-flex flex-wrap justify-content-center gap-4 py-4">
+              {searchResult?.map((course) => (
+                <SearchCard key={course.id} course={course} />
+              ))}
+            </Container>
           </div>
-        ))}
+        ) : (
+          <p className={styles.noSearchResult}>
+            Termo: <q>{searchTerm}</q> não encontrado
+          </p>
+        )}
+        <div className={styles.footer}>
+          <Footer />
+        </div>
       </main>
     </>
   );
